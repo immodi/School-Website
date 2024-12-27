b3f54702c7e40860008d73935eaf86975b50f9bb
immodi <ay727467@gmail.com>
2024-12-27 02:42:56 +0200

    why?

--- resources/views/backEnd/studentPanel/view_online_question.blade.php
+++ resources/views/backEnd/studentPanel/view_online_question.blade.php
@@ -80,7 +80,7 @@
                                         <td width="80%" class="pt-5">
                                             <h4>{{++$j.'.'}} {!! @$question->questionBank->question !!}</h4> 
                                             <div class="qustion_banner_img">
-                                                    <img src="{{ $question->questionBank->question_image }}" alt="">
+                                                    <img src="{{ @$question->questionBank->question_image }}" alt="">
                                                 </div>
                                             <!-- <h4>{{++$j.'.'}} {{@$question->questionBank->question}}</h4>  -->
                                             <!-- @if(@$question->questionBank->type == "MI")
