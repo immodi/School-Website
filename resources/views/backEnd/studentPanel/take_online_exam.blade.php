3c21bcf7fa0c547bc1de6bdf36b88d9a9de254b6
immodi <ay727467@gmail.com>
2024-12-28 21:26:54 +0200

    no new line please

--- resources/views/backEnd/studentPanel/take_online_exam.blade.php
+++ resources/views/backEnd/studentPanel/take_online_exam.blade.php
@@ -78,7 +78,7 @@
                                     
                                     <tr>
                                         <td width="80%" class="pt-5">
-                                            <h4>{{++$j.'.'}} {!! @$question->questionBank->question !!}</h4> 
+                                            {{++$j.'.'}} {!! @$question->questionBank->question !!}
                                             <div class="qustion_banner_img">
                                                 <img src="{{ @$question->questionBank->question_image }}" alt="">
                                             </div>
